535cbd7850adcab0652d0f2ea2c9a78f509e3bb1
AMANDEEP KAUR <49087637+amandeep25@users.noreply.github.com>
2020-05-26 23:38:39 +0530

    Update Register.java
    Added gmail verification

--- app/src/main/java/com/example/authenticatorapp/Register.java
+++ app/src/main/java/com/example/authenticatorapp/Register.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -16,9 +15,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
     EditText mName, mEmail, mPassword;
@@ -76,48 +78,47 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
                             //send verification link
-
                             FirebaseUser user = fAuth.getCurrentUser();
-                            user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>()){
-
+                            user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
-                                public void onSuccess(Void aVoid){
-                                    
-                                    Toast.makeText(Register.this, "Verification Email Has been Sent.", Toast.LENGTH_SHORT).show();
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(Register.this,"Verification Email has been sent",Toast.LENGTH_SHORT).show();
                                 }
-                            } ).addOnFailureListener(new OnFailureListener()){
+                            }) .addOnFailureListener(new OnFailureListener() {
                                 @Override
-                                public void onFailure(@NoNull Exception e){
-                                    Log.d(TAG, msg: "onFailure: Email not sent" + e.getMessage());
-                            }
-                        });
+                                public void onFailure(@NonNull Exception e) {
 
 
-                        Toast.makeText(Register.this, "User Created!", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                    Log.d("tag", "onFailure: Email not sent" + e.getMessage());
+                                }
+                            });
+                           
+
+                            Toast.makeText(Register.this, "User Created!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
 
 
-                    }
+                        }
                         else{
-                        Toast.makeText(Register.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                        progressBar2.setVisibility(View.INVISIBLE);
+                            Toast.makeText(Register.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar2.setVisibility(View.INVISIBLE);
+                        }
+
                     }
+                });
 
                 }
             });
 
-        }
-    });
-
         mtextView4.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            startActivity(new Intent(getApplicationContext(),Login.class));
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
 
-        }
-    });
+            }
+        });
 
 
 
-}
+    }
 }
